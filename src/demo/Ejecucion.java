218977a8ffefe0ce4425f1b506cab9bd33694514
JorgeAMezaCruz <mezacruz.2001@gmail.com>
2020-09-29 19:35:35 -0500

    Se agrega la variable m

--- src/demo/Ejecucion.java
+++ src/demo/Ejecucion.java
@@ -5,9 +5,11 @@ public class Ejecucion {
 		int x = 1;
 		int y = 2;
 		int z = 3;
+		int m = 4;
 		
 		System.out.println(x);
 		System.out.println(y);
 		System.out.println(z);
+		System.out.println(m);
 	}
 }
